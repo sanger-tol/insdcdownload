65ba1242a86cc764a17ac1dbd56bc4b1f2fad5eb
Matthieu Muffato <mm49@sanger.ac.uk>
2022-09-12 10:30:22 +0100

    The Python convention is not to wrap the code in functions

--- bin/masking_to_bed.py
+++ bin/masking_to_bed.py
@@ -3,29 +3,35 @@
 import sys
 
 # This script will generate a BED file of the masked regions a fasta file.
-in_gap = None
-with open(sys.argv[1]) as fh:
-    for line in fh:
-        line = line[:-1]
-        if line.startswith(">"):
-            if in_gap:
-                print("\t".join([chrom, str(start), str(i)]))
-                in_gap = False
-            i = 0  # Start coordinate is 0-based, included
-            chrom = line.split()[0][1:]
-        else:
-            for char in line:
-                if not in_gap and char.islower():
-                    in_gap = True
-                    start = i
-                elif in_gap and char.isupper():
-                    in_gap = False
-                    # end coordinate is 0-based, excluded
-                    print("\t".join([chrom, str(start), str(i)]))
-                else:
-                    pass
-                i += 1
+def fasta_to_bed(fasta):
 
-# Print mask close if on the last chromosome.
-if in_gap:
-    print("\t".join([chrom, str(start), str(i)]))
+    in_gap = None
+    with open(sys.argv[1]) as fh:
+        for line in fh:
+            line = line[:-1]
+            if line.startswith(">"):
+                if in_gap:
+                    print("\t".join([chrom, str(start), str(i)]))
+                    in_gap = False
+                i = 0  # Start coordinate is 0-based, included
+                chrom = line.split()[0][1:]
+            else:
+                for char in line:
+                    if not in_gap and char.islower():
+                        in_gap = True
+                        start = i
+                    elif in_gap and char.isupper():
+                        in_gap = False
+                        # end coordinate is 0-based, excluded
+                        print("\t".join([chrom, str(start), str(i)]))
+                    else:
+                        pass
+                    i += 1
+
+    # Print mask close if on the last chromosome.
+    if in_gap:
+        print("\t".join([chrom, str(start), str(i)]))
+
+
+if __name__ == "__main__":
+    fasta_to_bed(sys.argv[1])
