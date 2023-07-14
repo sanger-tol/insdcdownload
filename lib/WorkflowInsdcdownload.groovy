93863d6fab715b1d740b7bc7acd8d550e242eac0
Matthieu Muffato <mm49@sanger.ac.uk>
2023-07-14 10:23:27 +0100

    Template update for nf-core/tools version 2.8

--- lib/WorkflowInsdcdownload.groovy
+++ lib/WorkflowInsdcdownload.groovy
@@ -2,6 +2,7 @@
 // This file holds several functions specific to the workflow/insdcdownload.nf in the sanger-tol/insdcdownload pipeline
 //
 
+import nextflow.Nextflow
 import groovy.text.SimpleTemplateEngine
 
 class WorkflowInsdcdownload {
@@ -13,8 +14,7 @@ class WorkflowInsdcdownload {
         
 
         if (!params.fasta) {
-            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-            System.exit(1)
+            Nextflow.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
         }
     }
 
